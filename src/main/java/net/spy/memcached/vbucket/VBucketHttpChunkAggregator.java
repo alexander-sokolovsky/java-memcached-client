7a0e0dc8028ce6bb7ba395bc23756d5f0609dc54
Alexander Sokolovsky <alexander.sokolovsky.a@gmail.com>
2010-09-18 19:34:44 +0300

    Constructor for chunk aggregator

--- src/main/java/net/spy/memcached/vbucket/VBucketHttpChunkAggregator.java
+++ src/main/java/net/spy/memcached/vbucket/VBucketHttpChunkAggregator.java
@@ -1,7 +1,30 @@
 package net.spy.memcached.vbucket;
 
+import org.jboss.netty.handler.codec.http.HttpChunkAggregator;
+import org.jboss.netty.handler.codec.http.HttpMessage;
+
 /**
  * @author alexander.sokolovsky.a@gmail.com
  */
 public class VBucketHttpChunkAggregator {
+    private final int maxContentLength;
+    private volatile HttpMessage currentMessage;
+
+    /**
+     * Creates a new instance.
+     *
+     * @param maxContentLength
+     *        the maximum length of the aggregated content.
+     *        If the length of the aggregated content exceeds this value,
+     *        a {@link org.jboss.netty.handler.codec.frame.TooLongFrameException} will be raised.
+     */
+    public VBucketHttpChunkAggregator(int maxContentLength) {
+        if (maxContentLength <= 0) {
+            throw new IllegalArgumentException(
+                    "maxContentLength must be a positive integer: " +
+                    maxContentLength);
+        }
+        this.maxContentLength = maxContentLength;
+    }
+
 }
